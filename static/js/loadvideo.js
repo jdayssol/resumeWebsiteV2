499b7efb4674d70fcd78d096ee78869ad63b6dc7
joseph dayssol <joseph.dayssol@wibas-etarate.com>
2019-12-11 10:17:30 +0100

    correction of loadvideo

--- static/js/loadvideo.js
+++ static/js/loadvideo.js
@@ -1,20 +1,19 @@
  // Minified version of isMobile included in the HTML since it's small
  (function () {var f=/iPhone/i,j=/iPod/i,p=/iPad/i,g=/\bAndroid(?:.+)Mobile\b/i,i=/Android/i,d=/\bAndroid(?:.+)SD4930UR\b/i,e=/\bAndroid(?:.+)(?:KF[A-Z]{2,4})\b/i,c=/Windows Phone/i,h=/\bWindows(?:.+)ARM\b/i,k=/BlackBerry/i,l=/BB10/i,m=/Opera Mini/i,n=/\b(CriOS|Chrome)(?:.+)Mobile/i,o=/Mobile(?:.+)Firefox\b/i;function b($,a){return $.test(a)}function a($){var a=($=$||("undefined"!=typeof navigator?navigator.userAgent:"")).split("[FBAN");void 0!==a[1]&&($=a[0]),void 0!==(a=$.split("Twitter"))[1]&&($=a[0]);var r={apple:{phone:b(f,$)&&!b(c,$),ipod:b(j,$),tablet:!b(f,$)&&b(p,$)&&!b(c,$),device:(b(f,$)||b(j,$)||b(p,$))&&!b(c,$)},amazon:{phone:b(d,$),tablet:!b(d,$)&&b(e,$),device:b(d,$)||b(e,$)},android:{phone:!b(c,$)&&b(d,$)||!b(c,$)&&b(g,$),tablet:!b(c,$)&&!b(d,$)&&!b(g,$)&&(b(e,$)||b(i,$)),device:!b(c,$)&&(b(d,$)||b(e,$)||b(g,$)||b(i,$))||b(/\bokhttp\b/i,$)},windows:{phone:b(c,$),tablet:b(h,$),device:b(c,$)||b(h,$)},other:{blackberry:b(k,$),blackberry10:b(l,$),opera:b(m,$),firefox:b(o,$),chrome:b(n,$),device:b(k,$)||b(l,$)||b(m,$)||b(o,$)||b(n,$)},any:!1,phone:!1,tablet:!1};return r.any=r.apple.device||r.android.device||r.windows.device||r.other.device,r.phone=r.apple.phone||r.android.phone||r.windows.phone,r.tablet=r.apple.tablet||r.android.tablet||r.windows.tablet,r}window.isMobile=a();})();
 
- // My own arbitrary use of isMobile, as an example
  (function() {
-   // I only want to redirect iPhones, Android phones
+   var backgroundcover=document.querySelector("div#backgroundcover");
+   var video = document.querySelector('video#backgroundvideo');
+
    if (isMobile.apple.phone || isMobile.android.phone) {
-       var backgroundcover=document.querySelector("div#backgroundcover");
+     alert('isMobile')
        backgroundcover.style.zIndex=1
+       video.style.zIndex=-1
        backgroundcover.style.display="block"
-
-   }else{
-    var backgroundcover=document.querySelector("div#backgroundcover");
-    backgroundcover.style.zIndex=0
-
-    var video = document.querySelector('video#backgroundvideo');
-    var source = document.querySelector('source#backgroundvideosource');
+   }else{ 
+    alert('isComputer')
+    var source = document.querySelector('source#backgroundvideosource');  
+    backgroundcover.style.zIndex=-1
     video.style.zIndex=1
     source.setAttribute('src', "../static/images/video-background2.mp4");
     video.load(); 
